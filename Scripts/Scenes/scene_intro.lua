c1837d5f56f241397c315e4533d25b6f05a80d4c
Bli_Aik <haikun2333@126.com>
2026-06-12 13:28:40 +0800

    feat: Add callbacks for intro cutscenes

--- Scripts/Scenes/scene_intro.lua
+++ Scripts/Scenes/scene_intro.lua
@@ -2,12 +2,14 @@
 -- You can use this as a starting point for your own scenes.
 local SCENE = {}
 
-
-local function NewCutscene(path, duration, text)
+local function NewCutscene(path, duration, text, options)
+  options = options or {}
   return {
     path = path or "default.png",
     duration = duration or 3.0,
-    text = text or "undefined"
+    text = text or "undefined",
+    onDisplayStart = options.onDisplayStart,
+    onUpdate = options.onUpdate
   }
 end
 
@@ -15,21 +17,37 @@ local cutscenes = {
   NewCutscene("Cutscene/spr_introimage_0.png", 7.0,
     "[space:1, 2][space:2, 4][speed:0.95][voice:uifont.wav]Long ago, [wait:30]two races\nruled over Earth:\n[wait:30]HUMANS and MONSTERS."
   ),
-  NewCutscene("Cutscene/spr_introimage_1.png", 3.5),
-  NewCutscene("Cutscene/spr_introimage_2.png", 4.2)
+  NewCutscene("Cutscene/spr_introimage_1.png", 3.0,
+    "[space:1, 2][space:2, 4][speed:0.95][voice:uifont.wav]We all know what\nhappened next.[wait:30]\nSo..."
+  ),
+  NewCutscene("Cutscene/spr_introlast_0.png", 4.2, "", {
+    onDisplayStart = function(cutscene, sprite)
+      sprite:MoveTo(320, -100)
+      tween.CreateTween(
+        function(value)
+          sprite.y = value
+        end,
+        "linear", "", -100, 500, 60 * 4.2
+      )
+    end,
+
+    onUpdate = function(dt, cutscene, sprite)
+    end
+  })
 }
 
 local fade = 1.0
 
 --
+local current_index = 1
 
-local cutscene = sprites.CreateSprite(cutscenes[1].path, 0)
-cutscene:Scale(2, 2)
+local sprite = sprites.CreateSprite(cutscenes[current_index].path, 0)
+sprite:Scale(2, 2)
 
-local t = typers.CreateText(cutscenes[1].text, { 118, 319 }, 1.1, { 0, 0 }, "none")
+local t = typers.CreateText(cutscenes[current_index].text, { 118, 319 }, 1.1, { 0, 0 }, "none")
 
 local mask = masks.New("rectangle", 320, 170, 400, 220, 0)
-cutscene:SetStencils({ mask })
+sprite:SetStencils({ mask })
 
 -- In the original Undertale assets,
 -- the sprites at the end of the Pacifist Route (such as spr_asrielpanels_0.png) do not have black borders,
@@ -41,14 +59,18 @@ cutscene:SetStencils({ mask })
 
 
 -- This is a fake scene for testing purposes.
+
 function SCENE.load()
   -- Load any resources needed for this scene here.
   -- For example, you might load images, sounds, etc.
+  local first_scene = cutscenes[current_index]
+  if first_scene and first_scene.onDisplayStart then
+    first_scene.onDisplayStart()
+  end
 end
 
 -- This function is called to update the scene.
 local timer = 0
-local current_index = 1
 local state = "display"
 
 function SCENE.update(dt)
@@ -60,36 +82,45 @@ function SCENE.update(dt)
 
   timer = timer + dt;
 
-  local current_data = cutscenes[current_index]
+  local current_cutscene = cutscenes[current_index]
+
+  if current_cutscene and current_cutscene.onUpdate then
+    current_cutscene.onUpdate(dt, current_cutscene, sprite)
+  end
 
   if state == "fade_in" then
-    cutscene.alpha = math.min(timer / fade, 1.0)
+    sprite.alpha = math.min(timer / fade, 1.0)
 
     if timer >= fade then
       timer = 0
       state = "display"
     end
   elseif state == "display" then
-    cutscene.alpha = 1.0
+    sprite.alpha = 1.0
 
-    if timer >= current_data.duration then
+    if timer >= current_cutscene.duration then
       timer = 0
       state = "fade_out"
       t:SetText("")
     end
   elseif state == "fade_out" then
-    cutscene.alpha = 1.0 - math.min(timer / fade, 1.0)
+    sprite.alpha = 1.0 - math.min(timer / fade, 1.0)
 
     if timer >= fade then
       timer = 0
       current_index = current_index + 1
 
       if current_index <= #cutscenes then
-        cutscene:Set(cutscenes[current_index].path)
+        sprite:Set(cutscenes[current_index].path)
         t:SetText(cutscenes[current_index].text)
         state = "fade_in"
+
+        if current_cutscene.onDisplayStart then
+          current_cutscene.onDisplayStart(current_cutscene, sprite)
+        end
       else
         state = "finished"
+        scenes.switchTo("scene_logo")
       end
     end
   end
