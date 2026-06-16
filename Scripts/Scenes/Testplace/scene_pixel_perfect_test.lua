dd4e97502b5cdd4fcb8a2fc5984b4fba419373c7
Bli_Aik <haikun2333@126.com>
2026-06-16 13:21:19 +0800

    chore: Add moving and scaling sprites to the example scene

--- Scripts/Scenes/Testplace/scene_pixel_perfect_test.lua
+++ Scripts/Scenes/Testplace/scene_pixel_perfect_test.lua
@@ -12,6 +12,24 @@ local SPR_2X = sprites.CreateSprite("bullet.png", -1)
 SPR_2X.y = 160
 SPR_2X:Scale(2, 2)
 
+local SPR_MOVE = sprites.CreateSprite("bullet.png", -1)
+SPR_MOVE.x = 200
+SPR_MOVE.y = 110
+
+local SPR_MOVE_2X = sprites.CreateSprite("bullet.png", -1)
+SPR_MOVE_2X.x = 200
+SPR_MOVE_2X.y = 160
+SPR_MOVE_2X:Scale(2, 2)
+
+local SPR_SCALE = sprites.CreateSprite("bullet.png", -1)
+SPR_SCALE.x = 440
+SPR_SCALE.y = 110
+
+local SPR_SCALE_2X = sprites.CreateSprite("bullet.png", -1)
+SPR_SCALE_2X.x = 440
+SPR_SCALE_2X.y = 160
+SPR_SCALE_2X:Scale(2, 2)
+
 local beat_t = typers.DrawText("SRY but this image size \nhappens to be odd =)", { 50, 300 }, 1)
 -- This is a fake scene for testing purposes.
 function SCENE.load()
@@ -25,6 +43,14 @@ function SCENE.update(dt)
     -- For example, you might update animations, handle input, etc.
     SPR_1X.rotation = SPR_1X.rotation + 1
     SPR_2X.rotation = SPR_1X.rotation
+
+    local t = math.rad(SPR_1X.rotation)
+    SPR_MOVE.x = 200 + math.sin(t) * 80
+    SPR_MOVE_2X.x = SPR_MOVE.x
+    SPR_SCALE.xscale = 1 + math.sin(t) * 0.6
+    SPR_SCALE.yscale = SPR_SCALE.xscale
+    SPR_SCALE_2X.xscale = 2 + math.sin(t) * 1.2
+    SPR_SCALE_2X.yscale = SPR_SCALE_2X.xscale
 end
 
 -- This function is called to draw the scene.
